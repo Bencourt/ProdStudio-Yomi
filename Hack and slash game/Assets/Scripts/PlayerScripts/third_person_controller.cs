b0d135aa5e52a4298f7808d6d3175684430f2b18
bjl4618 <bjl4618@g.rit.edu>
2021-02-12 12:31:02 -0500

    initial unity project dump

---
+++ Hack and slash game/Assets/Scripts/PlayerScripts/third_person_controller.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class third_person_controller : MonoBehaviour
+{
+    
+    public CharacterController characterController;
+    public Transform playerCam;
+    public float walkSpeed = 6f;
+    public float sprintSpeed = 12f;
+    private float speed;
+    public float turnSmooth = 0.1f;
+    float turnSmoothVel;
+
+    Vector3 velocity;
+    public float jump = 10f;
+    public float fallGravity = -9.8f;
+    public float jumpGravity = -15f;
+    float gravity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    void Start()
+    {
+        speed = walkSpeed;
+        gravity = fallGravity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y > 0)
+        {
+            isGrounded = false;
+        }
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            gravity = fallGravity;
+        }
+        if(isGrounded && Input.GetAxisRaw("Jump") > .1f)
+        {
+            velocity.y = jump;
+            gravity = jumpGravity;
+        }
+        if(!isGrounded && velocity.y < 0)
+        {
+            gravity = fallGravity;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmooth);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+        if(Input.GetAxis("Sprint") >= .1f)
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+    }
+}
---
+++ Hack and slash game/Assets/Scripts/enemy.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxHealth = 100;
+    int currentHealth;
+    public CharacterController characterController;
+    public Vector3 knockbackDirection;
+
+    public Transform playerCheck;
+    public float attackRange = 1;
+    public LayerMask playerLayer;
+    public int damage = 10;
+    public int knockback = 10;
+    private bool attacked;
+
+
+    public float attackWindup = 1f;
+    float attackWindupTime = 0.0f;
+    public float attackRate = 2f;
+    float attackTime = 0.0f;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (knockbackDirection.magnitude > .1f)
+        {
+            characterController.Move(knockbackDirection * Time.deltaTime);
+        }
+        knockbackDirection = Vector3.Lerp(knockbackDirection, Vector3.zero, Time.deltaTime * 2f);
+
+        if (GetComponent<enemy_behavior>().enemyState == state.attack && !attacked && Time.time >= attackWindupTime)
+        {
+                Debug.Log("Attacked");
+                attacked = true;
+                Attack();
+                attackTime = Time.time + 1f / attackRate;
+        }
+        if (GetComponent<enemy_behavior>().enemyState == state.attack && Time.time >= attackTime && attacked)
+        {
+            Debug.Log("attacking");
+                attacked = false;
+                attackWindupTime = Time.time + 1f / attackWindup;
+        }
+    }
+
+    void Attack()
+    {
+        Collider[] hitPlayer = Physics.OverlapSphere(playerCheck.position, attackRange, playerLayer);
+
+        foreach (Collider player in hitPlayer)
+        {
+            //Debug.Log("hit player name:" + player.name);
+            player.GetComponent<player_combat>().TakeDamage(damage);
+            player.GetComponent<player_combat>().TakeKnockback(damage, transform);
+        }
+    }
+
+    public void TakeKnockback(int strenght, Transform source)
+    {
+        float horizontal = characterController.transform.position.x - source.position.x;
+        float vertical = characterController.transform.position.z - source.position.z;
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            knockbackDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward * strenght;
+            Debug.Log("Enemy Knocked Back");
+            Debug.Log(knockbackDirection.magnitude.ToString());
+
+        }
+    }
+
+    public void TakeDamage(int danage)
+    {
+        currentHealth -= danage;
+        if(GetComponent<enemy_behavior>().enemyState == state.attack)
+        {
+            attackTime = Time.time + 1f / attackRate;
+        }
+        if(currentHealth <= 0)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        //Debug.Log("enemy died!");
+        GetComponent<CharacterController>().enabled = false;
+        GetComponent<enemy_behavior>().enabled = false;
+        this.enabled = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (playerCheck == null)
+            return;
+        Gizmos.DrawWireSphere(playerCheck.position, attackRange);
+    }
+
+}
