3c19a3db2f3bc8293aac25c7fddc85695208d3d8
evh5516 <42793571+evh5516@users.noreply.github.com>
2021-02-26 18:19:55 -0500

    Basic Menu UI
    - Added simple menu system.
    - Only return to game does anything currently.
    - Hitting 'esc' triggers menu swap

---
+++ Hack and slash game/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour
+{
+    public bool pauseMenu;
+    public bool inGame;
+    public GameObject inGameUI;
+    public GameObject pauseMenuUI;
+    public Button returnToGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu = false;
+        inGameUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        inGame = true;
+        returnToGameButton.onClick.AddListener(ReturnToGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MenuSwap();
+    }
+
+
+    void MenuSwap()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (inGame)
+            {
+                pauseMenu = true;
+                inGame = false;
+                inGameUI.SetActive(false);
+                pauseMenuUI.SetActive(true);
+                return;
+            }
+            if (pauseMenu)
+            {
+                ReturnToGame();
+                return;
+            }
+        }
+    }
+
+    void ReturnToGame()
+    {
+        pauseMenu = false;
+        inGame = true;
+        pauseMenuUI.SetActive(false);
+        inGameUI.SetActive(true);
+    }
+}
